538c09d800039c95e888835b538dc40bc9c9f9c5
NINJAHATTORI004 <anshmittal132@gmail.com>
2025-02-10 15:59:11 +0530

    replace linear search with binary search implementation in demo class

--- demo.java
+++ demo.java
@@ -2,7 +2,7 @@ public class demo {
     public static void main(String[] args) {
         int num[] = { 5, 7, 9, 11, 13, 101 };
         int target = 101;
-        int result = linearsearch(num, target);
+        int result = binarysearch(num, target);
         if (result == -1) {
             System.out.println("Element not found");
         } else {
@@ -17,6 +17,22 @@ public class demo {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
                 return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarysearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return -1;
