78e4b350d29ed007e1a3a54be24255558ecddb2f
NINJAHATTORI004 <anshmittal132@gmail.com>
2025-04-10 02:01:04 +0530

    add maxSubArraySum class for calculating maximum subarray sum; add towerOfHanoi class skeleton

---
+++ maxsubarraysum.java
@@ -0,0 +1,19 @@
+public class maxsubarraysum {
+    public static void main(String[] args) {
+        int[] arr = {1, -2, 3, 4, -4, 6, -14, 8, 2};
+        System.out.println("Max sub array sum: " + maxSubArraySum(arr, arr.length));
+    }
+
+    public static int maxSubArraySum(int[] a, int size) {
+        int maxSoFar = 0, maxEndingHere = 0;
+        for (int i = 0; i < size; i++) {
+            maxEndingHere = maxEndingHere + a[i];
+            if (maxEndingHere < 0)
+                maxEndingHere = 0;
+            if (maxSoFar < maxEndingHere)
+                maxSoFar = maxEndingHere;
+        }
+        return maxSoFar; // Corrected return statement
+    }
+}
+    
