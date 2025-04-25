177aa9475f4c51d8ef18a9f4cb207ea1631020da
NINJAHATTORI004 <anshmittal132@gmail.com>
2025-04-25 14:22:48 +0530

    fix formatting in search output message for clarity

--- arraybasics/search.java
+++ arraybasics/search.java
@@ -5,7 +5,7 @@ public class search {
         boolean found=false;
         for (int i=0;i<arr.length;i++){
             if(arr[i]==key){
-                System.out.println("Element:"+key+"found at index"+i);
+                System.out.println("Element:"+key+" found at index: "+i);
                 found=true;
                 break;
 
