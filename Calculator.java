9baa6db8b68de12dc2db945572e72b880afa0da3
NINJAHATTORI004 <anshmittal132@gmail.com>
2025-06-26 15:17:15 +0530

    update Calculator class to handle division by zero and improve output formatting

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,35 @@
 import java.util.Scanner;
 public class Calculator
 {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		System.out.println("Sum is: " + (a + b));
-		System.out.println("Difference is: " + (a - b));
-
-		
-
-
-
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Sum is: " + (a + b));
+        System.out.println("Difference is: " + (a - b));
+        System.out.println("Product is: " + (a * b));
+        if (b != 0) {
+            System.out.println("Quotient is: " + (a / b));
+            System.out.println("Remainder is: " + (a % b));
+        } else {
+            System.out.println("Quotient is: Undefined (division by zero)");
+            System.out.println("Remainder is: Undefined (division by zero)");
+        }
+        System.out.println("Increment of a (post-increment): " + (a++));
+        System.out.println("Value of a after post-increment: " + a);
+        System.out.println("Decrement of b (pre-decrement): " + (--b));
+        System.out.println("Value of b after pre-decrement: " + b);
+        System.out.println("Increment of a (post-increment): " + (a++));
+        System.out.println("Value of a after post-increment: " + a);
+        System.out.println("Decrement of b (post-decrement): " + (b--));
+        System.out.println("Value of b after post-decrement: " + b);
+    }
 }
+
+
+	
+
+		
+
+
+
