d8ae4924e95cb1c91ed466c93e834c27ed9849d7
NINJAHATTORI004 <anshmittal132@gmail.com>
2025-06-27 16:00:23 +0530

    add nonstaticmethod class to demonstrate a non-static method in Java

--- arraybasics/java crt/factorial.java
+++ arraybasics/java crt/factorial.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class factorial {
     public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);  
         int fact=1;
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
@@ -12,3 +12,5 @@ public class factorial {
     }
 
 }
+
+
