73c994069377da555dc9c18b67a1065d67ae8f8f
NINJAHATTORI004 <anshmittal132@gmail.com>
2025-06-30 13:58:44 +0530

    add arraycreationinitialization class to demonstrate array declaration and initialization in Java

---
+++ arraybasics/java crt/arraycreationinitialization.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class arraycreationinitialization {
+    public static void main(String[] args) {
+        int[] arr = new int[5]; // Declare an array of integers with size 5
+        int[] primes = {2, 3, 5, 7}; // Initialize an array with values
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(primes));
+    }
+
+
+}
