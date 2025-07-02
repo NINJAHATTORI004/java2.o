d8ea264e2d56fd4817ae36cc9b272c685cf2b68a
NINJAHATTORI004 <anshmittal132@gmail.com>
2025-07-02 16:17:14 +0530

    refactor nonstaticmethod class to improve method organization and readability

--- arraybasics/java crt/nonstaticmethod.java
+++ arraybasics/java crt/nonstaticmethod.java
@@ -1,12 +1,23 @@
-
 public class nonstaticmethod {
     public void display() {
         System.out.println("This is a non-static method.");
     }
-    public static void main(String[] args) {
-        nonstaticmethod obj = new nonstaticmethod();
-        obj.display();       
 
+    public void greet(String name) {
+        System.out.println("Hello, " + name + "!");
     }
 
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        nonstaticmethod obj = new nonstaticmethod();
+        obj.display();
+
+        obj.greet("User");
+
+        int sum = obj.add(5, 7);
+        System.out.println("Sum: " + sum);
+    }
 }
