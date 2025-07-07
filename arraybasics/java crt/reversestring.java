3d190f2942fc1ad2e3f5a0d07dc37cfcbf36b17f
NINJAHATTORI004 <anshmittal132@gmail.com>
2025-07-07 12:02:17 +0530

    fix formatting in reversestring class output

--- arraybasics/java crt/reversestring.java
+++ arraybasics/java crt/reversestring.java
@@ -2,7 +2,7 @@ public class reversestring {
     public static void main (String[] args){
         String str="Hemanth";
         String rev= new StringBuilder(str).reverse().toString();
-        System.out.println("Original String: " + rev);
+        System.out.println("Original String:" + rev);
     }
 
 }
