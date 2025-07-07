4e572e52e78f862a1d00cf5dd8b4c98bdaf8a131
NINJAHATTORI004 <anshmittal132@gmail.com>
2025-07-07 12:20:46 +0530

    add panvalidator class to validate PAN format in Java

---
+++ arraybasics/java crt/panvalidator.java
@@ -0,0 +1,7 @@
+public class panvalidator {
+    public static void main(String[] args) {
+        String pan = "ABCDE1234F";
+        
+        System.out.println(pan.matches("[A-Z]{5}[0-9]{4}[A-Z]"));
+    }
+}
