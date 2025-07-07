d809397b5bf7f27795885ce62afe9112653338f9
NINJAHATTORI004 <anshmittal132@gmail.com>
2025-07-07 10:26:47 +0530

    clean up StringMethodsDemo class by removing inline comments for clarity

--- arraybasics/java crt/StringMethodsDemo.java
+++ arraybasics/java crt/StringMethodsDemo.java
@@ -1,28 +1,28 @@
 public class StringMethodsDemo {
     public static void main(String[] args){
         String str1 = "Hello";
-        System.out.println(str1.length()); // 5
+        System.out.println(str1.length()); 
 
         String str2 = "World";
-        System.out.println(str2.charAt(2)); // 'r'
+        System.out.println(str2.charAt(2)); 
 
         String str3 = "Hello World";
         String s1 = "Java";
         String s2 = "java";
-        System.out.println(s1.equals(s2)); // false
-        System.out.println(s1.equalsIgnoreCase(s2)); // true
+        System.out.println(s1.equals(s2)); 
+        System.out.println(s1.equalsIgnoreCase(s2)); 
 
         String a = "apple";
         String b = "banana";
-        System.out.println(a.compareTo(b)); // negative value
+        System.out.println(a.compareTo(b)); 
 
         String s3 = " Hello World ";
-        System.out.println(s3.trim()); // "Hello World"
+        System.out.println(s3.trim()); 
 
         String str5 = "banana";
-        System.out.println(str5.replace('a', 'o')); // "bonono"
-        System.out.println(str5.replaceAll("na", "123")); // "ba123123"
+        System.out.println(str5.replace('a', 'o')); 
+        System.out.println(str5.replaceAll("na", "123")); 
 
-        // You can add more String method demos here if needed
+      
     }
 }
